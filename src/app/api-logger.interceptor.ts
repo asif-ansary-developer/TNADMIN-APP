bc1705f5c2fcb7fdc059bd56f9bd595a144c7cf9
asif-ansary-developer <asifansary@rimes.int>
2026-03-24 13:36:45 +0530

    sherlock check changed and api service  is not edited to old server

--- src/app/api-logger.interceptor.ts
+++ src/app/api-logger.interceptor.ts
@@ -9,18 +9,22 @@ import {
 } from '@angular/common/http';
 
 import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class ApiLoggerInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    console.log("API_SHERLOCK  REQUEST");
-    console.log("API_SHERLOCK URL:", req.url);
-    console.log("API_SHERLOCK METHOD:", req.method);
-    console.log("API_SHERLOCK BODY:", JSON.stringify(req.body));
-    console.log("API_SHERLOCK HEADERS:", JSON.stringify(req.headers));
-
+    console.log("ENV CHECK:", JSON.stringify(environment));
+    console.log("INTERCEPTOR RUNNING");
+    const startTime = new Date().getTime();
+    if (environment.enableApiLogs) {
+      console.log("API_SHERLOCK  REQUEST");
+      console.log("API_SHERLOCK URL:", req.url);
+      console.log("API_SHERLOCK METHOD:", req.method);
+      console.log("API_SHERLOCK BODY:", JSON.stringify(req.body));
+      console.log("API_SHERLOCK HEADERS:", JSON.stringify(req.headers));
+    }
     return next.handle(req).pipe(
 
       tap({
@@ -29,26 +33,40 @@ export class ApiLoggerInterceptor implements HttpInterceptor {
 
           if (event instanceof HttpResponse) {
 
-            console.log("API_SHERLOCK   SUCCESS");
-            console.log("API_SHERLOCK URL:", req.url);
-            console.log("API_SHERLOCK STATUS:", event.status);
-            console.log("API_SHERLOCK RESPONSE:", JSON.stringify(event.body));
-            if (event.body == null || event.body.length === 0) {
-              console.warn("API_SHERLOCK ⚠️ EMPTY RESPONSE:", req.url);
-            }
+            if (environment.enableApiLogs) {
+              console.log("API_SHERLOCK   SUCCESS");
+              console.log("API_SHERLOCK URL:", req.url);
+              console.log("API_SHERLOCK STATUS:", event.status);
 
+              if (event.body && JSON.stringify(event.body).length < 2000) {
+                console.log("API_SHERLOCK RESPONSE:", JSON.stringify(event.body));
+
+                // if (!event.body) {
+              } else if (event.body == null || event.body.length === 0) {
+
+                console.warn("API_SHERLOCK ⚠️ EMPTY RESPONSE:", req.url);
+              }
+              else {
+                console.log("API_SHERLOCK RESPONSE: Large response received");
+              }
+
+
+
+            }
           }
 
         },
 
         error: (error: HttpErrorResponse) => {
 
-          console.log("API_SHERLOCK  ERROR");
-          console.log("API_SHERLOCK URL:", req.url);
-          console.log("API_SHERLOCK STATUS:", error.status);
-          console.log("API_SHERLOCK MESSAGE:", error.message);
-          console.log("API_SHERLOCK ERROR:", JSON.stringify(error.error));
+          if (environment.enableApiLogs) {
+            console.log("API_SHERLOCK  ERROR");
+            console.log("API_SHERLOCK URL:", req.url);
+            console.log("API_SHERLOCK STATUS:", error.status);
+            console.log("API_SHERLOCK MESSAGE:", error.message);
+            console.log("API_SHERLOCK ERROR:", JSON.stringify(error.error));
 
+          }
         }
 
       })
